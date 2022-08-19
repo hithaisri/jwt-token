7b333d9de600b17b929fe20f64b8b0850143a706
hithaisri <hithaisri.97@gmail.com>
2022-08-19 11:44:12 +0530

    implemented web security

--- src/main/java/com/token/demo/JwtTokenDemoApplication.java
+++ src/main/java/com/token/demo/JwtTokenDemoApplication.java
@@ -2,6 +2,9 @@ package com.token.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class JwtTokenDemoApplication {
@@ -10,4 +13,9 @@ public class JwtTokenDemoApplication {
 		SpringApplication.run(JwtTokenDemoApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
 }
